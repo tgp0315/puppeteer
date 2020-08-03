a7ab3432830f7a85d0672b0e7461f2bda270f4af
v_tianguoping <v_tianguoping@100tal.com>
2020-08-03 19:32:47 +0800

    feat:爬虫

---
+++ NetEasecloudmusic.js
@@ -0,0 +1,58 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+(async () => {
+  const browser = await puppeteer.launch({args: ["--no-sandbox"], headless: false, devtools: true});
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 2000,
+    height: 800
+  })
+  await page.goto("https://music.163.com/#");
+  await page.type(".txt.j-flag", "可不可以", {delay: 500});
+  // 回车
+  await page.keyboard.press("Enter");
+  // 获取歌曲列表
+  await page.waitFor(2000);
+  let iframe = await page.frames().find(f => f.name() === "contentFrame");
+  // console.log(iframe)
+  const SONG_LIST = await iframe.$(".srchsongst");
+  // console.log(SONG_LIST);
+  //获取歌曲 可不可以 的地址
+  const selectedSongHref = await iframe.evaluate(e => {
+    // console.log(e.childNodes);
+    const songList = Array.from(e.childNodes);
+    console.log(111111111, songList);
+    const idx = songList.findIndex(v => v.childNodes[1].innerText.replace(/\s/g, "") === "可不可以");
+    return songList[idx].childNodes[1].firstChild.firstChild.firstChild.href;
+  }, SONG_LIST)
+  // console.log(selectedSongHref);
+  //进入页面
+  await page.goto(selectedSongHref);
+  // 获取歌曲页面嵌套的iframe
+  await page.waitFor(2000);
+  iframe = await page.frames().find(f => f.name() === "contentFrame");
+  // 点击展开按钮
+  const unfoldButton = await iframe.$("#flag_ctrl");
+  await unfoldButton.click();
+  // 获取歌词
+  const LYRIC_SELECTOR = await iframe.$("#lyric-content");
+  const lyriCtn = await iframe.evaluate(e => {
+    return e.innerText;
+  }, LYRIC_SELECTOR);
+  // console.log(lyriCtn)
+  // 写入文件
+  let writerStream = fs.createWriteStream("歌词.text");
+  writerStream.write(lyriCtn, "UTF-8");
+  writerStream.end();
+  // 获取评论数量
+  const commentCount = await iframe.$eval(".sub.s-fc3", e => e.innerText);
+  console.log(commentCount)
+  // 获取评论
+  const commentList = await iframe.$eval('.m-cmmt .cmmts .itm', elements => {
+    const ctn = elements.map(v => {
+      return v.innerText.replace(/\s/g, '');
+    });
+    return ctn;
+  });
+  console.log(commentList);
+})()
--- screenshot.js
+++ screenshot.js
@@ -14,11 +14,22 @@ const puppeteer = require("puppeteer");
     width: 2000,
     height: 1000
   })
-  await page.goto("https://www.toutiao.com/")
+  await page.goto("https://www.jianshu.com/u/40909ea33e50")
+  // metrics获取页面打开过程的性能参数
   const metrics = await page.metrics()
   console.log(metrics);
   await page.screenshot({
-    path: "toutiao.png",
-    type: "png"
+    path: "jianshu.png",
+    type: "png",
+    // quality: 100,//只对jpg有效
+    fullPage: true,
+    //指定区域截图，clip和fullPage两者只能设置一个
+    // clip: {
+    //   x: 0,
+    //   y: 0,
+    //   width: 1000,
+    //   height: 40
+    // }
   })
+  await browser.close();
 })()
